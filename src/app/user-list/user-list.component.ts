5a2fed133420ef63d65883e65491461b3e0e2a00
Nicolas Guilherme Matheus <nicolas.gmop@gmail.com>
2022-04-04 23:22:58 -0300

    criando uma lista de usuários com @Input e CustomEvent

--- src/app/user-list/user-list.component.ts
+++ src/app/user-list/user-list.component.ts
@@ -1,23 +1,57 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, OnDestroy, OnInit, Output, SimpleChange, SimpleChanges } from '@angular/core';
 import { IUser } from './shared/interfaces/user.interface';
 
 @Component({
-  selector: 'app-user-list',
+  selector: 'nc-user-list',
   templateUrl: './user-list.component.html',
   styleUrls: ['./user-list.component.scss'],
 })
-export class UserListComponent implements OnInit {
+export class UserListComponent implements OnInit, OnDestroy, OnChanges {
 
-  @Input() users: IUser[] = [{id: 1,name: 'Nicolas', phone: '11985684547', address: { street: 'rua fulano' }}];
+  private _users: IUser[] = [];
 
-  @Output() onClickUser = new EventEmitter<IUser>();
+
+  /**
+   * O micro front end não entende o array de forma normal, então passamos um array no formado JSON
+   * ou seja passamos em string, e dps convertemos para objeto para que o javascript entenda.
+   */
+  @Input() set users(users: string | IUser[]) {
+    if( typeof users === 'string' ) {
+      const userParsed: IUser[] = JSON.parse(users) as IUser[];
+      this._users = userParsed;
+    } else {
+      this._users = users;
+    }
+  }
+
+  get users(): IUser[] {
+    return this._users;
+  }
+
+  @Input() token!: string;
 
   constructor() {}
 
-  ngOnInit(): void {}
 
+  ngOnChanges(changes: {[users: string]: SimpleChange}) {
+    // check the object "changes" for new data
+    console.log('users ngOnChanges=' + this.users);
+  }
+  
+  ngOnInit(): void {
+    window.addEventListener('user-list-on-click-user', this.onClickUser, true);
+  }
+  
+  onClickUser(event: any) {
+    console.log("disparei a função: ", event.detail);
+  }
+  
   onClickUserFunction(user: IUser) {
-    console.log(user);
-    this.onClickUser.emit(user);
+    const event = new CustomEvent('user-list-on-click-user', { detail: user });
+    window.dispatchEvent(event);
+  }
+  
+  ngOnDestroy(): void {
+    window.removeEventListener('user-list-on-click-user', this.onClickUser, true);
   }
 }
